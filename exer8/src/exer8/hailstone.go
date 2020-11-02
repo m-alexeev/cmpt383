dbddb2cc42a79d728f734eaf8d8797fe7a335075
B0uncyKnight <mishalxv35@gmail.com>
2020-11-02 11:50:27 -0800

    go

--- exer8/src/exer8/hailstone.go
+++ exer8/src/exer8/hailstone.go
@@ -13,10 +13,20 @@ func Hailstone(n uint) uint{
 
 
 func HailstoneSequenceAppend(n uint) []uint{
-	seq := make([]uint,1);
-	while (Hailstone(n) != 1){
+	seq := make([]uint,0);
+	seq = append(seq, n)
+	for (Hailstone(n) != 1){
 		seq = append (seq, Hailstone(n)); 
 		n = Hailstone(n); 
 	}
+	seq = append(seq, 1)
 	return seq	
 }
+
+func HailstoneSequenceAllocate(n uint) []uint{
+	seq := make([]uint, 0)
+	return seq
+}
+
+
+
--- exer8/src/main/main.go
+++ exer8/src/main/main.go
@@ -1,5 +1,15 @@
 package main
 
+import(
+	"fmt"
+	"../exer8"
+)
+
+
 func main(){
 	
+	fmt.Println(exer8.Hailstone(5))
+	fmt.Println(exer8.HailstoneSequenceAppend(5))
+
+
 }
