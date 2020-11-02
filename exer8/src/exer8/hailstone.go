1944038191891563fea806ce78872d528b1db298
B0uncyKnight <mishalxv35@gmail.com>
2020-11-02 12:45:08 -0800

    hailstone

--- exer8/src/exer8/hailstone.go
+++ exer8/src/exer8/hailstone.go
@@ -1,5 +1,4 @@
 package exer8
-
 // TODO: your Hailstone, HailstoneSequenceAppend, HailstoneSequenceAllocate functions
 
 func Hailstone(n uint) uint{
@@ -10,21 +9,36 @@ func Hailstone(n uint) uint{
 	}
 }
 
-
-
+// ! BenchmarkHailSeqAppend-6          656902              1797 ns/op
 func HailstoneSequenceAppend(n uint) []uint{
 	seq := make([]uint,0);
-	seq = append(seq, n)
-	for (Hailstone(n) != 1){
-		seq = append (seq, Hailstone(n)); 
+	for (n != 1){
+		seq = append (seq, n); 
 		n = Hailstone(n); 
 	}
-	seq = append(seq, 1)
+	seq = append(seq,1)
 	return seq	
 }
 
+
+// ! BenchmarkHailSeqAllocate-6       1493504               803 ns/op
 func HailstoneSequenceAllocate(n uint) []uint{
-	seq := make([]uint, 0)
+	var l uint = 0; 
+	var n1 = n
+	for (n1 != 1){
+		l += 1
+		n1 = Hailstone(n1)
+	}
+
+	seq := make ([]uint, l+1)
+
+	var i = 0
+	for (i < len(seq)){
+		seq[i] = n
+		n = Hailstone(n)
+		i += 1
+	}
+
 	return seq
 }
 
--- exer8/src/main/main.go
+++ exer8/src/main/main.go
@@ -10,6 +10,7 @@ func main(){
 	
 	fmt.Println(exer8.Hailstone(5))
 	fmt.Println(exer8.HailstoneSequenceAppend(5))
+	fmt.Println(exer8.HailstoneSequenceAllocate(5))
 
 
 }
