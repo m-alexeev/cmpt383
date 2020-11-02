9447c7b535f188880641ee73eb4b662e364454e2
B0uncyKnight <mishalxv35@gmail.com>
2020-11-02 13:06:20 -0800

    point

--- exer8/src/exer8/points.go
+++ exer8/src/exer8/points.go
@@ -1,3 +1,26 @@
 package exer8
-
+import (
+	"fmt"
+	"math"
+)
 // TODO: The Point struct, NewPoint function, .String and .Norm methods
+
+type Point struct{
+	x float64
+	y float64
+}
+
+func NewPoint(x float64, y float64)  Point{
+	return Point{x,y}
+}
+
+func (pt Point) String() string{
+	return fmt.Sprintf("(%v, %v)", pt.x, pt.y); 
+}
+
+func (pt Point) Norm() float64{
+	var norm float64 = pt.x*pt.x + pt.y*pt.y 
+	norm = math.Sqrt(norm)
+	
+	return norm
+}
--- exer8/src/main/main.go
+++ exer8/src/main/main.go
@@ -12,5 +12,10 @@ func main(){
 	fmt.Println(exer8.HailstoneSequenceAppend(5))
 	fmt.Println(exer8.HailstoneSequenceAllocate(5))
 
+	pt := exer8.NewPoint(3, 4.5)
+	fmt.Println(pt) // should print (3, 4.5)
+	fmt.Println(pt.String() == "(3, 4.5)") // should print true
 
+	pt1 := exer8.NewPoint(3, 4)
+	fmt.Println(pt1.Norm() == 5.0)
 }
