7c19faa4be78b190ef93f758bba5084b7514ddf1
B0uncyKnight <mishalxv35@gmail.com>
2020-11-05 19:57:49 -0800

    moved exercises to exer2.jl

--- Julia/exer2.jl
+++ Julia/exer2.jl
@@ -1,3 +1,5 @@
+using Dates 
+
 function divisors(num)
    return [i for i = 2:num-1 if num%i==0] 
 end 
@@ -23,8 +25,57 @@ function join(separ, strings)
     return ret
 end
 
+function merge(lst1, lst2)
+    arr = []
+    #basic merge 
+    while length(lst1) > 0 && length(lst2) >0
+        if lst1[begin] < lst2[begin]
+            append!(arr,lst1[begin])
+            lst1 = lst1[2:end]
+        else
+            append!(arr, lst2[begin])
+            lst2 = lst2[2:end]   
+        end
+    end  
 
-#=
+    #Concat the rest of the elemts
+    arr = [arr; lst1; lst2]
+
+    return arr
+end
+
+
+function mergeSort(lst)
+    if length(lst) <= 1
+        return lst
+    else 
+        # Split the array 
+        left = lst[1:length(lst)÷2]
+        right = lst[(length(lst)÷2)+1:end]
+        
+        #Recursively sort the halves
+        left = mergeSort(left)
+        right = mergeSort(right)
+        
+        #Merge the 2 sorted halves
+        merge(left,right)
+    end 
+end
+
+
+
+function isFriday(date)
+    return  (Dates.dayname(date) == "Friday") ? true : false
+end
+
+
+function isPrimeDay(date)
+    day = Dates.day(date)
+    return (length(divisors(day)) == 0) ? true : false 
+end
+
+
+
 # * Divisors Tests
 println(divisors(30))
 println(divisors(64))
@@ -43,4 +94,40 @@ println(join("+", ["a","b","c"]))
 println(join("+", ["a"]))
 println(join("+", []))
 
-=# 
+
+# * Testing Merge
+println("Testing Merge")
+println(merge([3,4],[5,6]))
+println(merge([1],[2]))
+println(merge([1],[2,3]))
+println(merge([1],[]))
+println(merge([],[1]))
+println(merge([1,2],[]))
+println(merge([],[1,2]))
+
+println("================")
+
+# * Testing MergeSort 
+println("Testing MergeSort")
+println(mergeSort([8,3,2,6]))
+println(mergeSort([8,2,6,2]))
+println(mergeSort([8,2,6]))
+println(mergeSort([2,1]))
+println(mergeSort([1]))
+println(mergeSort([]))
+
+ 
+#* Test if day is Friday
+println("Testing Friday\n")
+println("Tuesday 2020,10,20   ", isFriday(DateTime(2020,10,20))) #Tuesday
+println("Wednesday 2020,10,21 ", isFriday(DateTime(2020,10,21))) #Wednesday
+println("Thursday 2020,10,22  ", isFriday(DateTime(2020,10,22))) #Thursday
+println("Friday 2020,10,23    ", isFriday(DateTime(2020,10,23))) #Friday
+println("\n====================\n")
+
+#* Test if day is Prime 
+println("Testing PrimeDay\n")
+println("20 ", isPrimeDay(DateTime(2020,10,20))) #Tuesday
+println("21 ", isPrimeDay(DateTime(2020,10,21))) #Wednesday
+println("22 ", isPrimeDay(DateTime(2020,10,22))) #Thursday
+println("23 ", isPrimeDay(DateTime(2020,10,23))) #Friday
