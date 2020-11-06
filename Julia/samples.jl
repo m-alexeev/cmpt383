ca88b49eda4636103fe87e6cfee71bdde7e9563c
B0uncyKnight <mishalxv35@gmail.com>
2020-11-05 18:06:31 -0800

    finished

--- Julia/samples.jl
+++ Julia/samples.jl
@@ -2,15 +2,20 @@
 using Random
 function serialCalc(x, y)
     for i in 1:y
-        x += rand((1,10))
+        x += rand((1:10))
     end
     return x
 end 
+
 using Base.Threads
-##Multi threaded example, set number of threads to >1 to see results with "julia --threads 4"
+##Multi threaded example, set number of threads to >1 to see results 
+##On windows set the env variable to "set JULIA_NUM_THREADS=4" if on CMD 
+##On Linux "export JULIA_NUM_THREADS=4"
+##If the computer has at least 4 threads
+
 function parallelCalc(x,y)
     #Do serial calc for small y
-    if (y <= 1000000)
+    if (y <= 2000000)
        return serialCalc(x,y)
     #Otherwise split the process
     else
@@ -24,25 +29,70 @@ end
 
 ## To run this code, CSV and Plots must be installed for 
 ## Reading data
-## import Pkg; Pkg.add("CSV")
+## import Pkg; Pkg.add("CSV"); 
 using CSV
+using Statistics
 function linearRegression(filename)
     data = CSV.File(filename)
-    x = 0;y = 0; x_sq = 0; xy = 0;
-    n = 0
-    for row in data
-        y += row.LE; x += row.GDP
-        xy += row.LE + row.GDP; x_sq += row.GDP ^ 2
-        n +=1 
-    end
 
-    a = ((y * x_sq) - (x * xy)) / ((n*x_sq) - (x^2)) 
-    b = ((n*xy) - (x * y)) / ((n * x_sq) - x^2)
+    # Place rows into lists
+    x = [row.GDP for row in data]
+    y = [row.LE for row in data]
 
-    print("Regression Line with  Y-Int = $(a), Slope = $(b)")
+    n = length(x)
+    ## Means of the X and Y
+    mean_x = mean(x)
+    mean_y = mean(y)
+
+    #Cross deviation
+    xy = sum([x[i]*y[i] for i in 1:n]) - (n * mean_x * mean_y)
+    xx = sum([x[i]^2 for i in 1:n]) - (n * mean_x^2)
+
+    a = xy / xx
+    b = mean_y - (a * mean_x)
+
+    println("Regression Line with  Y-Int = $(b), Slope = $(a)")
+
 end 
 
+using Printf
+## Julias Random function number distribution 
+function randomDistribution(nums, maxNum)
+    rands = Dict{Integer,Integer}()
 
-#linearRegression("SampleData.csv")
-println(@time serialCalc(0, 10000000))
-println(@time parallelCalc(0, 10000000))
+    for i in 1:nums
+        gen = rand((1:maxNum))
+        push!(rands, gen => get(rands,gen,0) + 1)
+
+    end
+
+    percents = Dict{Integer,Float32}()
+    for key in rands
+        push!(percents, key[1]=>get(rands,key[1],0) / nums)
+    end
+    
+    #Sort Dictionary
+    percents = sort(collect(percents), by= x->x[1])
+    println("Distribution of $(nums) random values in the range: 1:$(maxNum)
+===================================================")
+    for key in percents
+        println("Value: $(key[1]) => $(key[2])%")
+    end 
+
+end
+
+
+println("Regression Line Calculation")
+linearRegression("SampleData.csv")
+println("\nComparing serial vs parallel Processing ")
+println("SET THE NUMBER OF THREADS > 1")
+println(@time serialCalc(0, 100000000))
+println(@time parallelCalc(0, 100000000))
+
+randomDistribution(1000,10)
+println()
+randomDistribution(100000,10)
+println()
+randomDistribution(10000000,10)
+
+
