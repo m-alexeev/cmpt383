2ea8a09be9a261b2a044652aaca02c3479595268
Mikhail Alexeev <mishalxv35@gmail.com>
2020-11-14 17:47:08 -0800

    add create func

--- a3/src/work_queue/work_queue.go
+++ a3/src/work_queue/work_queue.go
@@ -11,27 +11,35 @@ type WorkQueue struct {
 
 // Create a new work queue capable of doing nWorkers simultaneous tasks, expecting to queue maxJobs tasks.
 func Create(nWorkers uint, maxJobs uint) *WorkQueue {
+	//* initialize struct;
 	q := new(WorkQueue)
-	// TODO: initialize struct; start nWorkers workers as goroutines
+	// TODO start nWorkers workers as goroutines
+	q.Results = make (chan interface{}, maxJobs)
+	q.Jobs = make (chan Worker, maxJobs) 
+	for i := uint(0); i < nWorkers; i ++ {
+		go q.worker()
+	}
+	
 
 	return q
 }
 
 // A worker goroutine that processes tasks from .Jobs (until shut down).
 func (queue WorkQueue) worker() {
-	// TODO: Listen on the .Jobs channel for incoming tasks. For each task...
-	// TODO: run tasks by calling .Run(),
-	// TODO: send the return value back on Results channel.
-	// TODO: Exit (return) when .Jobs is closed.
+	// TODO: Listen on the .Jobs channel for incoming tasks. 
+	// ! For each task...
+		// * run tasks by calling .Run(),
+		// * send the return value back on Results channel.
+		// * Exit (return) when .Jobs is closed.
 }
 
 func (queue WorkQueue) Enqueue(work Worker) {
-	// TODO: put the work into the Jobs channel so a worker can find it and start the task.
+	// * put the work into the Jobs channel so a worker can find it and start the task.
 	queue.Jobs <- work
 }
 
 func (queue WorkQueue) Shutdown() {
-	// TODO: close .Jobs and remove all remaining jobs from the channel.
+	// * close .Jobs and remove all remaining jobs from the channel.
 	close(queue.Jobs)
 	for range queue.Jobs{}
 }
