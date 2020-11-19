b51d6a5fd879965d9c6a3076a9d38bd57f1bd80b
B0uncyKnight <mishalxv35@gmail.com>
2020-11-19 15:39:50 -0800

    fixed warn

--- exer10/src/main/main.go
+++ exer10/src/main/main.go
@@ -14,11 +14,11 @@ func main(){
 	// }
 
 
-	pt:= exer10.Point{3,4}
+	pt:= exer10.Point{X:3,Y:4}
 	exer10.TurnDouble(&pt,3*math.Pi/2)
 	fmt.Println(pt.String())
 
-	tri := exer10.Triangle{exer10.Point{1,2}, exer10.Point{-3,4}, exer10.Point{5,-6}}
+	tri := exer10.Triangle{A:exer10.Point{X:1,Y:2}, B:exer10.Point{X:-3,Y:4}, C:exer10.Point{X:5,Y:-6}}
 	exer10.TurnDouble(&tri, math.Pi)
 	fmt.Println(tri.String())
 }
