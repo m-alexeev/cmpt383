2fbf3e0f30e4aea2a5ecd837497cd16091730496
B0uncyKnight <mishalxv35@gmail.com>
2020-11-19 12:25:44 -0800

    set up fib

--- exer10/src/exer10/fib.go
+++ exer10/src/exer10/fib.go
@@ -3,3 +3,8 @@ package exer10
 func Fib(n uint, cutoff int ) uint{
 	return uint(0)
 }
+
+
+func Fibbonaci(n uint) uint{
+	return Fib(n, 0)
+}
--- exer10/src/main/main.go
+++ exer10/src/main/main.go
@@ -1,5 +1,14 @@
 package main
 
+
+import (
+	"../exer10"
+	"fmt"
+)
+
 func main(){
+
+	n := uint(10)
+	fmt.Println(exer10.Fibbonaci(n))
 	
 }
