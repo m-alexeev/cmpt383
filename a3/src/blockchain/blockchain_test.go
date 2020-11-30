8b59b92f610e0eca4820c6260603e0288dba053c
Mikhail Alexeev <mishalxv35@gmail.com>
2020-11-29 17:14:08 -0800

    passed instructor tests

--- a3/src/blockchain/blockchain_test.go
+++ a3/src/blockchain/blockchain_test.go
@@ -1,10 +1,41 @@
 package blockchain
 
 import (
-	// "github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+	"encoding/hex"
 )
+
+
+func TestInstructorValues(t *testing.T){
+	b0 := Initial(7)
+	b0.Mine(1)
+	if b0.Proof != 385{
+		t.Error("Block 0 Proof is not same as instructor proof")
+	}
+	if hex.EncodeToString(b0.Hash) != "379bf2fb1a558872f09442a45e300e72f00f03f2c6f4dd29971f67ea4f3d5300"{
+		t.Error("Block 0 Hash is not the same as the instructor hash")
+	}
+
+	b1 := b0.Next("this is an interesting message")
+	b1.Mine(1)
+	if b1.Proof != 20{
+		t.Error("Block 1 Proof is not same as instructor proof")
+	}
+	if hex.EncodeToString(b1.Hash) != "4a1c722d8021346fa2f440d7f0bbaa585e632f68fd20fed812fc944613b92500"{
+		t.Error("Block 1 Hash is not the same as the instructor hash")
+	}
+
+	b2 := b1.Next("this is not interesting") 
+	b2.Mine(1)
+	if b2.Proof != 40{
+		t.Error("Block 2 Proof is not same as instructor proof")
+	}
+	if hex.EncodeToString(b2.Hash) != "ba2f9bf0f9ec629db726f1a5fe7312eb76270459e3f5bfdc4e213df9e47cd380"{
+		t.Error("Block 2 Hash is not the same as the instructor hash")
+	}
+}
+
 
 func TestSimpleMining(t *testing.T){
 	
@@ -49,23 +80,18 @@ func TestSimpleMining(t *testing.T){
 	}
 
 }
-
-func TestMiningComplex(t *testing.T){
-
-}
-
-
 func TestMiningSpeed(t *testing.T){
 
 	marginErr := 1.15
 
+	//Run sequential mine for benchmark
 	b0s := Initial(20)
 	seqStart := time.Now()
 	b0s.MineSequential()
 	seqEnd := time.Now()
 	targetTime := float64(seqEnd.Sub(seqStart))
 
-	
+	//Run concurrent miner with 1 worker (should be close to sequential)
 	b0c := Initial(20)	
 	concStart := time.Now()
 	b0c.Mine(1)
@@ -77,6 +103,7 @@ func TestMiningSpeed(t *testing.T){
 		t.Error("1 Worker concurrent mining is taking too long")
 	}
 
+	//Run concurrent miner with 4 workers, should be faster than sequential
 	b1c := b0c.Next("Some Hash")
 	concStart = time.Now()
 	b1c.Mine(4)
@@ -86,7 +113,6 @@ func TestMiningSpeed(t *testing.T){
 	if timeTaken > targetTime { 
 		t.Error ("Concurrent mining is slower than sequential")
 	}
-
 
 }
 
